ea61d3454a216318bd3cc82b5494bedd7fa97867
luislustosa <165705506+luislustosa@users.noreply.github.com>
2026-05-19 14:33:26 -0300

    Colocando comentarios

--- app.py
+++ app.py
@@ -10,14 +10,13 @@ CORS(app)
 @app.route('/detect-emotion', methods=['POST'])
 def detect_emotion():
     try:
-        # Recebe a imagem do frontend
         image_data = request.files['image']
-        image_array = np.frombuffer(image_data.read(), np.uint8)
+        image_array = np.frombuffer(image_data.read(), np.uint8) # Recebe imagem do frontend ( camera.html ) 
         image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
         
-        # Detecta emoção em 3 linhas
-        result = DeepFace.analyze(image, actions=['emotion'], enforce_detection=False)
-        emotion = result[0]['dominant_emotion']
+        
+        result = DeepFace.analyze(image, actions=['emotion'], enforce_detection=False) #Modelo deepface vai analizar a imagem e trazer a emocao mais forte
+        emotion = result[0]['dominant_emotion']                         
         emotions = result[0]['emotion']
         
         return jsonify({'emotion': emotion, 'all_emotions': emotions})
